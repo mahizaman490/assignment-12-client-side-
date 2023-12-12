f08f32677c3c84c817fa5efbdac323341b0ad4ef
Mahi Zaman <mahizaman490@gmail.com>
2023-12-12 15:04:17 +0600

    updated a feature

--- src/pages/Home/TopDeliveryManSec/BenefitsSection.jsx
+++ src/pages/Home/TopDeliveryManSec/BenefitsSection.jsx
@@ -45,8 +45,8 @@ const BenefitsSection = () => {
     <div className="stat-figure text-secondary">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
     </div>
-    <div className="stat-title">New Users</div>
-    <div className="stat-value">4,200</div>
+    <div className="stat-title">Pending Services</div>
+    <div className="stat-value">1,200</div>
     <div className="stat-desc">↗︎ 400 (22%)</div>
   </div>
   
@@ -54,8 +54,8 @@ const BenefitsSection = () => {
     <div className="stat-figure text-secondary">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
     </div>
-    <div className="stat-title">New Registers</div>
-    <div className="stat-value">1,200</div>
+    <div className="stat-title">New Service Requests</div>
+    <div className="stat-value">200</div>
     <div className="stat-desc">↘︎ 90 (14%)</div>
   </div>
   
